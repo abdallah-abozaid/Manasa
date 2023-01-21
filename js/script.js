0f5868e1c3d2a75528ba7de8b26174f4790e731f
abdalah <abood.99.10.osa@gmail.com>
2023-01-21 17:33:26 +0300

    add new pages with it languages

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ window.onscroll = function () {
     TopBtn.classList.add("hide");
   }
 };
+
+// ------------------------------------------------------------
+// Info links page
+let btns = document.querySelectorAll(".InfoLinks .btns button");
+let Contents = document.querySelectorAll(".InfoLinks .contant >div");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = function () {
+    // remove active class from all btns + Hide all content
+    for (let n = 0; n < btns.length; n++) {
+      btns[n].classList.remove("Active");
+      Contents[n].classList.add("Hide");
+    }
+    // add active class to clicked btn + delete Hide class from clicked content
+    btns[i].classList.add("Active");
+    Contents[i].classList.remove("Hide");
+  };
+}
